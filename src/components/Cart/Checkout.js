f5d956f4e9b806ae3b537f2803408d0cc1e757db
Berkay Anduv <sygrok@hotmail.com>
2023-10-12 20:51:27 +0300

    Form&Validity

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,104 @@
+import { useRef, useState } from "react";
+import classes from "./Checkout.module.css";
+
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim() === 5;
+
+const Checkout = (props) => {
+  const [formInputValidity, setFormInputValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
+
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+
+    setFormInputValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalCode,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredCityIsValid &&
+      enteredPostalCodeIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+  };
+
+  //Conditional Error classes
+  const nameClass = `${classes.control} ${
+    formInputValidity.name ? "" : classes.invalid
+  }`;
+
+  const streetClass = `${classes.control} ${
+    formInputValidity.street ? "" : classes.invalid
+  }`;
+
+  const postalClass = `${classes.control} ${
+    formInputValidity.postalCode ? "" : classes.invalid
+  }`;
+
+  const cityClass = `${classes.control} ${
+    formInputValidity.city ? "" : classes.invalid
+  }`;
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={nameClass}>
+        <label htmlFor="name">Your Name</label>
+        <input ref={nameInputRef} type="text" id="name" />
+        {!formInputValidity.name && <p>Please enter a valid name!</p>}
+      </div>
+      <div className={streetClass}>
+        <label htmlFor="street">Street</label>
+        <input ref={streetInputRef} type="text" id="street" />
+        {!formInputValidity.street && <p>Please enter a valid street name!</p>}
+      </div>
+      <div className={postalClass}>
+        <label htmlFor="postal">Postal Code</label>
+        <input ref={postalCodeInputRef} type="text" id="postal" />
+        {!formInputValidity.postalCode && (
+          <p>Please enter a valid postal code!</p>
+        )}
+      </div>
+      <div className={cityClass}>
+        <label htmlFor="city">City</label>
+        <input ref={cityInputRef} type="text" id="city" />
+        {!formInputValidity.city && <p>Please enter a valid city name!</p>}
+      </div>
+      <div className={classes.actions}>
+        <button onClick={props.onCancel} type="text">
+          Cancel
+        </button>
+        <button type="submit" className={classes.submit}>
+          Confirm
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
